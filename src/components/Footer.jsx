fa2906111986759bf0562750a657a35041207292
Abraham Anderson <abrahama@uia.no>
2022-02-12 14:41:32 +0100

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>Created by Abraham Anderson in {new Date().getFullYear()}</p>
+    </footer>
+  );
+}
+
+export default Footer;
